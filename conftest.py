c2c96cf85b65ce1fedc1518484710bfc2ecbfc78
QAFed <143259597+QAFed@users.noreply.github.com>
2024-09-29 10:00:13 +0300

    delete courier done!

--- conftest.py
+++ conftest.py
@@ -30,3 +30,13 @@ def create_courier():
     courier_sign_up = CourierSignUp(gen_courier_data.get_data_for_sign_up_courier())
     courier_sign_up.request()
     return gen_courier_data
+
+@pytest.fixture
+def create_n_sign_in():
+    gen_courier_data = GenCourierData()
+    courier_sign_up = CourierSignUp(gen_courier_data.get_data_for_sign_up_courier())
+    courier_sign_up.request()
+    sign_in_courier = CourierSignIn(gen_courier_data.get_data_for_sign_in_courier())
+    sign_in_courier.request()
+    gen_courier_data.sidn_in_id = sign_in_courier.response.json()["id"]
+    return gen_courier_data
--- endpoints/delete_courier_delete.py
+++ endpoints/delete_courier_delete.py
@@ -12,3 +12,6 @@ class CourierDelete:
 
     def check_status_code(self, status_code):
         assert self.response.status_code == status_code
+
+    def check_response_json(self, expect_dict):
+        assert self.response.json() == expect_dict
